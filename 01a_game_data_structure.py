899aaebbd3399fcc1117466650d5614130707d6f
GenuinelyAref <aref.osman18@gmail.com>
2024-08-21 14:35:48 +1200

    Add methods for checking game completion status

--- 01a_game_data_structure.py
+++ 01a_game_data_structure.py
@@ -41,13 +41,33 @@ def print_game_data(var_island_data):
         print("\n", end="")
 
 
+# check if an island has all its bridges built
+def check_island_bridges_complete(var_island_data, var_live_data, var_pos_row, var_pos_col):
+    return var_island_data[var_pos_row][var_pos_col] == var_live_data[var_pos_row][var_pos_col]
+
+
+# check if all islands' bridges are built
+def check_all_island_bridges_complete(var_island_data, var_live_data):
+    # get dimensions of grid
+    var_rows = len(var_island_data)
+    var_cols = len(var_island_data[0])
+    # check each island
+    for r in range(0, var_rows):
+        for c in range(0, var_cols):
+            # if any island hasn't had all its bridges built, return false
+            if var_island_data[r][c] != var_live_data[r][c]:
+                return False
+    # if no island hasn't had all its bridges built (all islands have all their bridges built), return true
+    return True
+
+
 # set grid size
 rows, cols = (9, 6)
 
-# initialise 2D array
-island_data = initialise_game_data(rows, cols)
-# island_data[3][3] = 4
-# insert game data
-island_data = insert_game_data(island_data)
+# initialise 2D array and insert game data
+island_data = insert_game_data(initialise_game_data(rows, cols))
 
-print_game_data(island_data)
+# initialise live user data 2D array
+live_data = initialise_game_data(rows, cols)
+
+print("Game finished: {}".format(check_all_island_bridges_complete(island_data, live_data)))
