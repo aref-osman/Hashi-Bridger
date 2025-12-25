f89cf18f320085c526426b9face65325c297d30f
Aref Osman <60583307+aref-osman@users.noreply.github.com>
2025-12-25 23:57:06 +1300

    style: squeeze code lines together :)

--- src/components/CardinalDirection.java
+++ src/components/CardinalDirection.java
@@ -9,15 +9,11 @@ public enum CardinalDirection {
     private final int value;
 
     // constructor
-    private CardinalDirection(int value) {
-        this.value = value;
-    }
+    private CardinalDirection(int value) {this.value = value;}
 
-    public int value() {
-        return value;
-    }
-
-    public int oppositeValue() {
-        return (value + 2) % 4;
-    }
+    public int value() {return value;}
+    public CardinalDirection opposite() {return CardinalDirection.values()[(value + 2) % 4];}
+    public Orientation getOrientation() {
+        return switch (this) {case NORTH -> Orientation.VERTICAL; case EAST -> Orientation.HORIZONTAL;
+            case SOUTH -> Orientation.VERTICAL; case WEST -> Orientation.HORIZONTAL;};}
 }
